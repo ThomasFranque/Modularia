2b6547afbdf9fd03457299452723d95c53bc63d6
Thomas the Franque <tfranque@gmail.com>
2020-07-24 23:33:07 +0100

    Add dev keys

---
+++ Assets/Scripts/DevKeys.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevKeys : MonoBehaviour
+{
+    bool _paused;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+        else if (Input.GetKeyDown(KeyCode.P))
+            TogglePause();
+    }
+
+    private void TogglePause()
+    {
+        _paused = !_paused;
+        if (_paused)
+            Time.timeScale = 0.0f;
+        else
+            Time.timeScale = 1;
+    }
+}
